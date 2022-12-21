8fb507f341cf14ba2bd9d9d83943eb875d559263
antonymwakiacha <tonywugoma@gmail.com>
2022-12-21 20:47:32 +0300

    Uploading my First flutter app project

---
+++ android/app/src/main/kotlin/org/pwaniteknowgalz/my_first_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package org.pwaniteknowgalz.my_first_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/intro.dart
@@ -0,0 +1,11 @@
+
+import 'package:flutter/material.dart';
+
+class Intro  extends StatelessWidget {
+  const Intro ({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
