599c99aa79915d685d1e1f16ae6fe9b6fe640948
antonymwakiacha <tonywugoma@gmail.com>
2022-12-29 18:44:18 +0300

    Added a text widget on the image

--- lib/screens/intro.dart
+++ lib/screens/intro.dart
@@ -1,11 +1,46 @@
-
 import 'package:flutter/material.dart';
 
-class Intro  extends StatelessWidget {
-  const Intro ({Key? key}) : super(key: key);
+class Intro extends StatelessWidget {
+  const Intro({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.all(20),
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              //FIRST CONTAINER
+              Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height * 0.22,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage("images/forestbg.jpg"),
+                      fit: BoxFit.cover,
+                      colorFilter: ColorFilter.mode(
+                          Colors.black.withOpacity(0.5),
+                          BlendMode
+                              .darken), //applying a dark filter on the image
+                    ),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Center(
+                    child: Text("Basic Widgets",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold),
+                    ),
+                  ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
