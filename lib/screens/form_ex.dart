6c631bd7070a0a645fc7f9b66893ae57340e8dcc
antonymwakiacha <tonywugoma@gmail.com>
2023-01-02 11:30:39 +0300

    Created the form widget to display the form fields and handling the validation

--- lib/screens/form_ex.dart
+++ lib/screens/form_ex.dart
@@ -33,6 +33,66 @@ class _FormExState extends State<FormEx> {
       ),
       body: Padding(
         padding: EdgeInsets.all(16),
+        //form
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: <Widget>[
+              Text(
+                "Form-Validation In Flutter ",
+                style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+              ),
+              //styling
+              SizedBox(
+                height: MediaQuery.of(context).size.width * 0.1,
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'E-Mail'),
+                keyboardType: TextInputType.emailAddress,
+                onFieldSubmitted: (value) {
+                  //Validator
+                },
+                validator: (value) {
+                  if (value!.isEmpty ||
+                      !RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                          .hasMatch(value)) {
+                    return 'Enter a valid email!';
+                  }
+                  return null;
+                },
+              ),
+              //box styling
+              SizedBox(
+                height: MediaQuery.of(context).size.width * 0.1,
+              ),
+              //text input
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Password'),
+                keyboardType: TextInputType.emailAddress,
+                onFieldSubmitted: (value) {},
+                obscureText: true,
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Enter a valid password!';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.width * 0.1,
+              ),
+              TextButton(
+                child: Text(
+                  "Submit",
+                  style: TextStyle(
+                    fontSize: 24.0,
+                  ),
+                ),
+                onPressed: () => _submit(),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
